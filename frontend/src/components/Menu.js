b0a405fc249d5c45448940135d65b1a3594614e9
orbich67 <orbidan@bk.ru>
2021-12-12 21:11:04 +0300

    Practical task lesson 2

---
+++ frontend/src/components/Menu.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Menu = () => {
+    return (
+        <ul>
+            <li><a href="#">Пункт меню 1</a></li>
+            <li><a href="#">Пункт меню 2</a></li>
+            <li><a href="#">Пункт меню 3</a></li>
+        </ul>
+    )
+}
+
+export default Menu;
